f99e298cde7a85e862c47eb42706dac324641549
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-04-27 16:23:17 +0100

    Render Space, Board and Token.

--- js/Board.js
+++ js/Board.js
@@ -27,4 +27,11 @@ class Board {
     return spaces;
   }
 
+  drawHTMLBoard() {
+    for (let column of this.spaces) {
+      for (let space of column) {
+        space.drawSVGSpace();
+      };
+    }
+  }
 }

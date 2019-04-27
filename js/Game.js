d7a4e4251e3ecdd8590b5ca1b951384190a33c56
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-04-27 15:39:21 +0100

    Connecting with the DOM.

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,24 @@
 class Game {
+  constructor() {
+    this.board = new Board();
+    this.players = this.createPlayer();
+    this.ready = false;
+  }
 
+  /** 
+   * Creates two player objects
+   * @return  {Array}    An array of two Player objects.
+   */
+
+  createPlayer() {
+    const players = [
+      new Player('Player 1', 'yellow', 1, true),
+      new Player('Player 2', 'red', 2)
+    ]
+    return players;
+  }
+
+  startGame() {
+    alert('clicked in Game object');
+  }
 }
